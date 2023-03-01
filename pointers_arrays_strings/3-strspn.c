7fc83ed737f3227b4fd1d89b9c340234cded5f2c
Chulumanco Poswa <chulumancop@gmail.com>
2023-03-01 00:50:55 -0800

    strd

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,36 @@
 #include "main.h"
 
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
-void _puts(char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
+ */
 
-	_putchar('\n');
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
 }
