604d396092d3f0a3b32572b1fb719c14b5a59646
Chulumanco Poswa <chulumancop@gmail.com>
2023-03-22 23:10:19 -0700

    fix

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,17 +8,16 @@
   * Return: sum of all arguments
   */
 int sum_them_all(const unsigned int n, ...)
-{
-	unsigned int i, result;
-	va_list ap;
-
-	if (n == 0)
-		return (0);
-	va_start(ap, n);
-	for (i = result = 0; i < n; i++)
-	{
-		result += va_arg(ap, int);
-	}
-	va_end(ap);
-	return (result);
+  (
+   unsigned int i, result;
+   va_list ap;
+   if (n == 0)
+   return (0);
+   va_start(ap, n);
+   for (i = result = 0; i < n; i++)
+   {
+    result += va_arg(ap, int);
+   }
+   va_end(ap);
+   return (result);
 }
